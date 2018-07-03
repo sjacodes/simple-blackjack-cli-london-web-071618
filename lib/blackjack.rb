003adb81ea1df062c550b6e97f1d1522ce28b0a5
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 18:45:46 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -59,24 +59,11 @@ def runner
   card total = initial_round
   until card_total > 21
     card_total =  hit?(card_total)
-    
-    display_card_total
+    display_card_total(card_total)
   end
-  end_game
+  end_game(card_total)
 end
 
 def runner
-  welcome #introduce the game
-  sleep 0.75
-  card_total = initial_round #determine your current total & tell user what number they currently have
-  until card_total > 21 #continue game until user loses
-    card_total = hit?(card_total)
-    display_card_total(card_total)
-  end
-  end_game(card_total) #prints out once user has lost
-end
-
-runner 
-    
   
 runner 
