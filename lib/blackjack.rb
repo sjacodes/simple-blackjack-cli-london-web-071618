38e20717e4c813caea27be56efbe83c684fbf721
Sarah Jacob <smjacob91@gmail.com>
2018-07-03 18:10:59 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,8 +35,9 @@ def hit?(current_card_total)
   prompt_user
   user_input = get_user_input
   if user_input = 's'
-    return card_total
-  
+    puts card_total
+  elsif user_input = 'h'
+    
   
   # code hit? here
 end
